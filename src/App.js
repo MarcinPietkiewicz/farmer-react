517073980f1b5f63a12c0ffdcb6d88c65572d864
Marcin <marcin.p.dev@gmail.com>
2021-05-31 12:37:03 +0200

    Initial converting html to REACt components without JS functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav>
+        <div id="menuToggle">
+      
+          <input type="checkbox" />
+          
+          <span></span>
+          <span></span>
+          <span></span>
+          
+          <ul id="menu">
+            <a href="#technical-section"><li>Play anywhere</li></a>
+            <a href="#social-section"><li>Develop social skills</li></a>
+            <a href="#news-section"><li>News</li></a>
+            <a id="menu-buy" href="#shop-section"><li>BUY</li></a>
+          </ul>
+        </div>
+      </nav>
+
+
+
+<div className="hero-section">
+<main className="bg1">
+    <div id="hero">
+      <div id="hero-title">Farmer Olaf</div>
+<div id="hero-title-desc">Play anywhere.</div>
+
+<div id="tldr-container">
+<div className="tldr-section"><a className="tldr" href="#technical-section">Play anywhere</a></div>
+<div className="tldr-section"><a className="tldr" href="#social-section">Easy to play, hard to master</a></div>
+<div className="tldr-section"><a className="tldr" href="#social-section">Develop social skills</a></div>
+</div>
+</div>
+</main>
+</div>
+    
     </div>
   );
 }
-
+}
 export default App;
