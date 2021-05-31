9a660e6bd0fda72d0dd068e61464dd4b414a588e
Marcin <marcin.p.dev@gmail.com>
2021-05-31 12:02:25 +0200

    Initial react commit

---
+++ public/assets/script.js
@@ -0,0 +1,19 @@
+window.onload = function(){ 
+   document.getElementById("hideAll").style.display = "none"; 
+
+let title = document.getElementById('hero-title');
+title.classList.add('transition');
+
+let titleText = document.getElementById('hero-title-desc');
+titleText.classList.add('transitionTwo'); 
+
+  //  let animationElements = document.getElementsByClassName('tran');
+  //  for (let i=0, i<animationElements.length,i++ )
+   
+  //  animationElements.forEach(function(element){
+  //    element.classList.add('transition');
+  //    console.log(animationElements[0]);
+  //    console.log(animationElements[1]);
+  //  });
+}
+
